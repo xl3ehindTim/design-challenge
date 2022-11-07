738b59d527b26c0e345a33e8a74b478826bef4d4
xl3ehindTim <32740573+xl3ehindTim@users.noreply.github.com>
2022-11-07 14:32:09 +0100

    Added Api

---
+++ api/check-postgres.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+import psycopg2
+
+# Simple code to check if database connection is live
+
+while True:
+    try:
+        conn = psycopg2.connect("dbname='%s' user='%s' host='%s' password='%s'" % (
+            os.environ['POSTGRES_DB'],
+            os.environ['POSTGRES_USER'],
+            os.environ['POSTGRES_HOST'],
+            os.environ['POSTGRES_PASSWORD']))
+        break
+    except Exception as ex:
+        print(ex)
+        time.sleep(1)
---
+++ api/src/users/rest.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import get_user_model
+from django.core.mail import EmailMultiAlternatives
+from django.dispatch import receiver
+from django.template.loader import render_to_string
+from django.urls import reverse
+from django_rest_passwordreset.signals import reset_password_token_created
+from rest_framework import viewsets, serializers
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+            "is_active",
+            "is_superuser",
+            "is_staff",
+        )
+        depth = 1
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+    ordering_fields = "__all__"
+    filterset_fields = ("id",)
+    search_fields = ("email", "first_name", "last_name")
+
+    @action(detail=False, methods=["get"])
+    def get_current_user(self, request, *args, **kwargs):
+        return Response(UserSerializer(request.user).data)
+
+    def perform_create(self, serializer):
+        data_password = self.request.data.get("password")
+        data_confirm_password = self.request.data.get("confirm_password")
+
+        if data_password and data_password != data_confirm_password:
+            raise Exception("Passwords do not match")
+
+        user = serializer.save()
+
+        if data_password:
+            user.set_password(data_password)
+            user.save()
+
+    def perform_update(self, serializer):
+        data_password = self.request.data.get("password")
+        data_confirm_password = self.request.data.get("confirm_password")
+
+        if data_password and data_password != data_confirm_password:
+            raise Exception("Passwords do not match")
+
+        user = serializer.save()
+
+        if data_password:
+            user.set_password(data_password)
+            user.save()
+
+
+# register viewsets for the rest-api
+def register(restrouter):
+    restrouter.register(r"users", UserViewSet)
