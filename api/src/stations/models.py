2d2b776db4bec368d5da836e8ad644ab918117cb
xl3ehindTim <32740573+xl3ehindTim@users.noreply.github.com>
2022-11-18 13:36:37 +0100

    Removed CalenderEntry model and added CompatibleStationLink model

--- api/src/stations/models.py
+++ api/src/stations/models.py
@@ -1,23 +1,33 @@
 from django.db import models
 
 
-# Station database model
-class Station(models.Model):
-    beneCode = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
+class CompatibleStationLink(models.Model):
+    """
+    Database model for linking compatible Stations
+    """
+    source = models.ForeignKey(
+        "stations.Station", on_delete=models.CASCADE, related_name="+"
+    )
+    target = models.ForeignKey(
+        "stations.Station", on_delete=models.CASCADE, related_name="+"
+    )
 
     def __str__(self):
-        return self.name
+        return f"{self.source.name} <-> {self.target.name}"
+
+
+class Station(models.Model):
+    """
+    Station database model
+    """
+    beneCode = models.CharField(max_length=255)
+    name = models.CharField(max_length=255) 
+    compatible_stations = models.ManyToManyField(
+        "self", through=CompatibleStationLink, blank=True
+    )
     
-# Calender bookable entry database model
-class CalenderEntry(models.Model):
-    from_station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name="from_station", blank=True, null=True)
-    to_station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name="to_station", blank=True, null=True)
-    date = models.DateField()
-    price = models.DecimalField(max_digits=25, decimal_places=2)
-    category = models.CharField(max_length=250)
-    departure_date = models.DateTimeField()
-    arrival_date = models.DateTimeField()
+    def __str__(self):
+        return self.name
 
 
 class BookingEntry(models.Model):
--- api/src/stations/rest.py
+++ api/src/stations/rest.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework import viewsets
 import datetime
-from .models import Station, CalenderEntry, BookingEntry
+from .models import Station, CompatibleStationLink, BookingEntry
 
 
 class StationSerializer(serializers.ModelSerializer):
@@ -10,9 +10,9 @@ class StationSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class CalenderEntrySerializer(serializers.ModelSerializer):
+class CompatibleStationLinkSerializer(serializers.ModelSerializer):
     class Meta:
-        model = CalenderEntry
+        model = CompatibleStationLink
         fields = "__all__"
 
 
@@ -31,26 +31,12 @@ class StationViewSet(viewsets.ModelViewSet):
     )
 
 
-class CalenderEntryViewSet(viewsets.ModelViewSet):
-    queryset = CalenderEntry.objects.all()
-    serializer_class = CalenderEntrySerializer
+class CompatibleStationLinkViewSet(viewsets.ModelViewSet):
+    queryset = CompatibleStationLink.objects.all()
+    serializer_class = CompatibleStationLinkSerializer
 
     def get_queryset(self):
-        # Filter for today/after today
-        today = datetime.date.today()
-        queryset = CalenderEntry.objects.filter(date__gte=today)
-
-        # If route has been supplied filter
-        param_route = self.request.GET.get("route")
-
-        if param_route:
-            route = param_route.split("-")
-            
-            # From-To
-            queryset = queryset.filter(
-                from_station__beneCode=route[0],
-                to_station__beneCode=route[1],
-            )
+        queryset = CompatibleStationLink.objects.all()
 
         return queryset
 
@@ -87,5 +73,4 @@ class BookingEntryViewSet(viewsets.ModelViewSet):
 def register(restrouter):
     # Register routes to the API
     restrouter.register(r"stations", StationViewSet)
-    restrouter.register(r"calender-entries", CalenderEntryViewSet)
     restrouter.register(r"booking-entries", BookingEntryViewSet)
