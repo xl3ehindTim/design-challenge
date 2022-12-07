9ee45c396aa93c6ffd505167c735850ab89ad347
LaurieH2000 <464015@student.fontys.nl>
2022-12-07 11:32:25 +0100

    added navbar

--- talk-to-me-frontend/src/features/layout/AppLayout.tsx
+++ talk-to-me-frontend/src/features/layout/AppLayout.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { getUser } from '../auth/services/user.service';
+import logo from "../../assets/img/GTLogo.png";
+import Image from 'next/image'
 
 function AppLayout({ children }: any) {
   const user = getUser();
 
   return (
     <>
-      <div>Navigatiebalk hier</div>
+      <div className="navbar">
+        <div className="logo">
+        <Image src={logo} height="40px" width="100px" alt="GreenTravels" />
+        </div>
+      </div>
 
-      <div style={{ flexGrow: 1, maxWidth: "100%" }}>{children}</div>
+      <div style={{ maxWidth: "100%" }}>{children}</div>
     </>
   );
 }
